506545282299224f60078cfb5b55ebe5677c910a
Hao Pan <hpan@thoughtworks.com>
2019-08-01 09:30:46 +0800

    init project

---
+++ src/main/java/com/thoughtworks/workshoptest/WorkshopTestApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.workshoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopTestApplication.class, args);
+	}
+
+}
