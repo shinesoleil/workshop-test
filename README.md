ed8293019ee433aded78c0b45bb88950b227cff2
Hao Pan <hpan@thoughtworks.com>
2019-08-01 09:40:06 +0800

    should return ok when getting products in controller

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Spring Boot Workshop
 
+Task 1:  
+should return ok when getting products - Controller
+Estimation: 5 minutes  
+Actual Time: 4 minutes  
+Learning: none
---
+++ src/test/java/com/thoughtworks/workshoptest/controllers/ProductControllerTest.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.workshoptest.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void should_return_ok_when_getting_products_in_controller() throws Exception {
+    mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+  }
+}
